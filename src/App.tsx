20e352890a7789ecbdcc1711547768f5759efef1
Angelika Wicka <wicka.angelika@gmail.com>
2023-07-03 17:12:35 +0200

    New component WordParser for excr 30-06-23

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,10 @@
 import "./App.css";
+import WordParser from "./Components/WordParser/WordParser";
 
 function App() {
-  const expression = "coffee";
-
-  function countCharacters(strng: string): number {
-    const numberOfCharacters = strng.length;
-    return numberOfCharacters;
-  }
-
-  function isEven(nmbr: number): boolean {
-    return nmbr % 2 === 0;
-  }
-
-  function getParityMessage(bln: boolean): string {
-    if (bln === true) {
-      return "number is even";
-    } else {
-      return "number is not even";
-    }
-  }
-
-  function processString(str?: string | number | boolean): string | undefined {
-    if (str !== undefined) {
-      return `Expression "${str}" has ${countCharacters(
-        expression
-      )} characters in it and this ${getParityMessage(
-        isEven(countCharacters(expression))
-      )}.`;
-    } else {
-      return "Expression not known, nothing to count.";
-    }
-  }
-
   return (
     <>
-      <h3>Introduction to TS</h3>
-      <div>{processString(expression)}</div>
-      <div>{processString()}</div>
+      <WordParser />
     </>
   );
 }
---
+++ src/Components/WordParser/WordParser.tsx
@@ -0,0 +1,42 @@
+function WordParser() {
+  const expression = "coffee";
+
+  function countCharacters(strng: string): number {
+    const numberOfCharacters = strng.length;
+    return numberOfCharacters;
+  }
+
+  function isEven(nmbr: number): boolean {
+    return nmbr % 2 === 0;
+  }
+
+  function getParityMessage(bln: boolean): string {
+    if (bln === true) {
+      return "number is even";
+    } else {
+      return "number is not even";
+    }
+  }
+
+  function processString(str?: string | number | boolean): string | undefined {
+    if (str !== undefined) {
+      return `Expression "${str}" has ${countCharacters(
+        expression
+      )} characters in it and this ${getParityMessage(
+        isEven(countCharacters(expression))
+      )}.`;
+    } else {
+      return "Expression not known, nothing to count.";
+    }
+  }
+
+  return (
+    <>
+      <h3>Introduction to TS</h3>
+      <div>{processString(expression)}</div>
+      <div>{processString()}</div>
+    </>
+  );
+}
+
+export default WordParser;
